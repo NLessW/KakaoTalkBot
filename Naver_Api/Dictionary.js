84dac11acb63da569a1a8c246b7cc3cb32ae98ae
Less <63160418+NLessW@users.noreply.github.com>
2023-07-17 02:59:48 +0900

    Update Dictionary.js

--- Naver_Api/Dictionary.js
+++ Naver_Api/Dictionary.js
@@ -1,23 +1,29 @@
+const allsee = "\u200b".repeat(500);
+importPackage(org.jsoup);
+
 function response(room, msg, sender, isGroupChat, replier) {
+  function stripTags(text) {
+    return text.replace(/<\/?b>/g, "");
+  }
+
   function searchDictionary(query) {
     try {
       var url = "https://openapi.naver.com/v1/search/encyc.json?query=" + query;
       var response = org.jsoup.Jsoup.connect(url)
-        .header("X-Naver-Client-Id", "{CLIENT_ID}")
-        .header("X-Naver-Client-Secret", "{CLIENT_SECRET}")
+        .header("X-Naver-Client-Id", "YOUR_CLIENT_ID")
+        .header("X-Naver-Client-Secret", "YOUR_CLIENT_SECRET")
         .ignoreContentType(true)
         .get();
 
       var dictionaryResult = JSON.parse(response.body().text());
 
       if (dictionaryResult.items && dictionaryResult.items.length > 0) {
-        var resultText = "사전 검색 결과:\n\n";
+        var resultText = "사전 검색 결과\n\n";
 
         for (var i = 0; i < dictionaryResult.items.length; i++) {
           var item = dictionaryResult.items[i];
-          resultText += "제목: " + item.title.replace("<\/b>", "") + "\n";
-          resultText += "링크: " + item.link + "\n";
-          resultText += "설명: " + item.description + "\n\n";
+          resultText += "제목: " + stripTags(item.title) + "\n";
+          resultText += "설명: " + stripTags(item.description) + "\n\n";
         }
 
         return resultText.trim();
@@ -29,9 +35,9 @@ function response(room, msg, sender, isGroupChat, replier) {
     }
   }
 
-  if (msg.startsWith("/사전 ")) {
+  if (msg.startsWith("!사전 ")) {
     var query = msg.substring(4).trim();
     var result = searchDictionary(query);
-    replier.reply(result);
+    replier.reply("["+query+"] 사전 검색 결과"+allsee+"\n"+result);
   }
 }
